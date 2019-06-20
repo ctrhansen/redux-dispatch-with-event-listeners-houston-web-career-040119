3ddf57c85a20da7e256058b2a764d215fc1e2f70
Thomas_Hansen <ctrhansen@yahoo.com>
2019-06-20 17:13:26 -0500

    done w/decrease button

--- reducer.js
+++ reducer.js
@@ -1,1 +1,35 @@
 // add code snippets from README
+let state
+ 
+function reducer(state = {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    case 'DECREASE_COUNT':
+      return {count: state.count - 1};
+    default:
+      return state;
+  }
+}
+
+function dispatch(action){
+    state = reducer(state, action)
+    render()
+  }
+
+function render(){
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+  }
+
+button.addEventListener('click',
+function(){
+    dispatch({type: 'INCREASE_COUNT'})
+})
+
+button2.addEventListener('click',
+function(){
+	dispatch({type: 'DECREASE_COUNT'})
+})
+
+dispatch({type: '@@INIT'})
